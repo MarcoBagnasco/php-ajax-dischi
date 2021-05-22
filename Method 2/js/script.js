9a4bd177db22135dfb942d9bb326e3d4022505f9
MarcoBagnasco <bagna.hbk@gmail.com>
2021-05-22 13:23:25 +0200

    add genre select and improvements

--- Method 2/js/script.js
+++ Method 2/js/script.js
@@ -8,6 +8,7 @@ const app = new Vue({
     data: {
         diskList: [],
         searchString: '',
+        selectedGenre: 'all',
     },
     created(){
         this.callDisk();
@@ -19,31 +20,44 @@ const app = new Vue({
         callDisk(){
             const dataURL = `${window.location.href}scripts/get-disk.php`;
             
-            axios.get(dataURL)
+            axios.get(dataURL, {
+                params: {
+                    author: this.searchString,
+                    genre: this.selectedGenre,
+                }
+            })
             .then(res => {
-                this.diskList = res.data;
+                this.diskList = res.data.albums;
             })
             .catch(err => {
                 console.log(err);
             });
         },
-        /**
-         * Call API Filtered Disk
-         */
-        searchAuthor(){
-            const dataURL = `${window.location.href}scripts/get-disk-filtered-by-author.php`;
 
-            axios.get(dataURL, {
-                params: {
-                    author: this.searchString,
-                }
-            })
-            .then(res => {
-                this.diskList = res.data;
-            })
-            .catch(err => {
-                console.log(err);
-            });
-        }
+        /**
+         * Clear Author Search
+         */
+        clearAuthor(){
+            this.searchString = '';
+            this.callDisk();
+        },
+        // /**
+        //  * Call API Filtered Disk
+        //  */
+        // searchAuthor(){
+        //     const dataURL = `${window.location.href}scripts/get-disk-filtered-by-author.php`;
+
+        //     axios.get(dataURL, {
+        //         params: {
+        //             author: this.searchString,
+        //         }
+        //     })
+        //     .then(res => {
+        //         this.diskList = res.data;
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+        // }
     },
 });
--- Method 2/scripts/get-disk.php
+++ Method 2/scripts/get-disk.php
@@ -3,7 +3,49 @@
 // Include Disk Database
 include __DIR__ . '/../partials/database.php';
 
-header('Content-Type: application/json');
+// Create Genres Array
+$genres = [];
+foreach($database as $data){
+    if(!in_array(strtolower($data['genre']), $genres))
+    $genres[] = strtolower($data['genre']);
+}
 
-echo json_encode($database);
+// Get Query String
+$author = empty($_GET['author']) ? '' : strtolower($_GET['author']);
+$genre = empty($_GET['genre']) ? 'all' : $_GET['genre'];
+
+// Filtered Array
+$filtered = [];
+
+// Filter by Genre
+if($genre !== 'all' && in_array($genre, $genres)){
+    foreach($database as $data){
+        if(strtolower($data['genre']) === $genre){
+            $filtered[] = $data;
+        }
+    }
+} else {
+    $filtered = $database;
+}
+
+// Filter by Author
+if($author !== ''){
+    // Temporary Array
+    $temp = [];
+
+    foreach($filtered as $item){
+        if(strpos(strtolower($item['author']), $author) !== false){
+            $temp[] = $item;
+        }
+    }
+    $filtered = $temp;
+} 
+
+$returnData = [
+    'genreList' => $genres,
+    'albums' => $filtered,
+];
+
+header('Content-Type: application/json');
+echo json_encode($returnData);
 ?>
